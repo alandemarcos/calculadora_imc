8322e890a4927f38e69efcf7fd66804c6a88c1c8
Alan Demarcos <alandemarcos@msn.com>
2022-02-05 19:05:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './app.css';
 export default function App() {
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
-  const [mensagem, setMensagem] = useState('dfgd');
+  const [mensagem, setMensagem] = useState('');
 
   function calcularIMC(){
     const alt = altura / 100;
@@ -14,13 +14,13 @@ export default function App() {
     // alert(imc.toFixed(2)); 
 
     if(imc < 18.6){
-      alert("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
+      setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
     }else if(imc >= 18.6 && imc < 24.9){
-      alert("Peso ideal! Seu IMC: " + imc.toFixed(2))
+      setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2))
     }else if(imc >= 24.9 && imc < 34.9){
-      alert("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2))
+      setMensagem("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2))
     }else if(imc > 34){
-      alert("Cuidado! Você está obeso(a)! Seu IMC: " + imc.toFixed(2))
+      setMensagem("Cuidado! Você está obeso(a)! Seu IMC: " + imc.toFixed(2))
     }
   }
 
